c26316dbe8de539fd88bbc66e5bdaaa4144264cb
Felix Cheysson <felix@cheysson.fr>
2019-10-24 19:11:49 +0200

    Added unitary tests for spectral density function.

--- src/exponential.cpp
+++ src/exponential.cpp
@@ -1,40 +1,48 @@
 #include "model.hpp"
 #include "utils.hpp"
 
+// Model is given by its intensity :
+// Intensity = param(0) + param(1) * int h(t-u) dN(u)
+// where h(t) = param(2) * exp( -param(2) * t )
+// Then:
+// param(0): baseline intensity of the Hawkes process
+// param(1): reproduction rate /in [0,1]
+// param(2): time scale parameter of the exponential fertility distribution
+
 ////////////////////////////////////////////////
 // Methods for long term mean and its derivative
 
 double Exponential::mean() {
     return param(0) / ( 1.0 - param(1) );
-};
+}
 
 arma::vec Exponential::dmean() {
     double denom = 1.0 / ( 1.0 - param(1) );
     arma::vec grad = { denom, param(0) * denom * denom, 0 };
     return grad;
-};
+}
 
 arma::mat Exponential::ddmean() {
     double denom = 1.0 / ( 1.0 - param(1) );
     double denom2 = denom * denom;
     arma::mat hess = {  {   0.0,                  denom2, 0.0},
-    {denom2, 2*param(0)*denom2*denom, 0.0},
-    {   0.0,                     0.0, 0.0} };
+                        {denom2, 2*param(0)*denom2*denom, 0.0},
+                        {   0.0,                     0.0, 0.0} };
     return hess;
-};
+}
 
 //////////////////////////////////////////////////////////////
 // Methods for time- and frequency-domain excitation functions
 
 arma::vec Exponential::h( arma::vec x ) {
     return param(1) * param(2) * arma::exp( - param(2) * x );
-};
+}
 
 arma::cx_vec Exponential::H( arma::vec xi ) {
     arma::vec factor = param(1) * param(2) / ( param(2)*param(2) + xi%xi );
     arma::cx_vec zeta = arma::cx_vec( factor * param(2), - factor % xi );
     return zeta;
-};
+}
 
 arma::cx_mat Exponential::dH( arma::vec xi ) {
     arma::cx_mat grad = arma::zeros<arma::cx_mat>( xi.n_elem, param.n_elem );
@@ -42,7 +50,7 @@ arma::cx_mat Exponential::dH( arma::vec xi ) {
     grad.col(1) = param(2) * denom;
     grad.col(2) = i * param(1) * (xi % denom % denom);
     return grad;
-};
+}
 
 arma::cx_cube Exponential::ddH( arma::vec xi ) {
     arma::cx_cube hess = arma::zeros<arma::cx_cube>( param.n_elem, param.n_elem, xi.n_elem );
@@ -52,4 +60,4 @@ arma::cx_cube Exponential::ddH( arma::vec xi ) {
     hess.tube(2,1) = grad12;
     hess.tube(2,2) = -2.0 * param(1) * grad12 % denom;
     return hess;
-};
+}
--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -4,18 +4,25 @@ test_that("Methods for spectral densities work", {
     model = new(Exponential)
     model$param=c(1,.5,2)
 
-    # Excitation function
-    h0 =
+    # For w = 0
+    expect_equal(   model$f(0)[1,], 8 )
+    expect_equal(  model$df(0)[1,], c(8, 48, 0) )
+    expect_equal(model$ddf(0)[,,1], matrix(c( 0,  48, 0,
+                                             48, 384, 0,
+                                              0,   0, 0),
+                                           nrow=3, ncol=3) )
 
+    # For w = 1
+    s2 = (2.0 * sin(.5))^2  # sinc²(1.0 / 2.0)
+    expect_equal(   model$f(1)[1,], 5*s2)
+    expect_equal(  model$df(1)[1,], c(5*s2, 20*s2, 1.5*s2))
+    expect_equal(model$ddf(1)[,,1], matrix(c(     0,  20*s2,  1.5*s2,
+                                              20*s2, 100*s2,   11*s2,
+                                             1.5*s2,  11*s2, -.75*s2),
+                                           nrow=3, ncol=3) )
 
-    # Correct bins in R
-    ti <- seq(0, T, length.out=length+1)
-    bin <- sapply(1:length, function(i) {
-        sum(x > ti[i] & x < ti[i+1])
-    })
-
-    # "expect" methods
-    expect_equal( length(ddata$counts), length )
-    expect_equal( as.numeric(ddata$counts), bin )
-    expect_equal( ddata$binsize, T/length )
+    # # For w = 2
+    # s2 = sin(1)^2 # sinc²(2.0 / 2.0)
+    # expect_equal(   model$f(2)[1,], 3.2*s2)
+    # expect_equal(  model$df(2)[1,], c(3.2*s2, 8.96*s2, .96*s2) )
 })
