6d82746f921b2ace8e9358e75860537815cb2808
Felix Cheysson <felix@cheysson.fr>
2020-01-22 18:47:07 +0100

    hawkes.R:hawkes OK. hawkes.R:intensity unfinished.

--- R/hawkes.R
+++ R/hawkes.R
@@ -19,26 +19,21 @@
 #' @export
 #'
 #' @examples
-#' # Simulate an inhomogeneous Poisson process with function intensity 1 + sin(x)
+#' # Simulate an inhomogeneous Poisson process with function intensity 1 + sin(x) (bounded by 2)
 #' x <- inhpois(T=10, fun=function(y) {1 + sin(y)}, M=2)
-#' plot(x)
+#' # Simulate a homogeneous Poisson process with intensity 3
+#' x <- inhpois(T=10, M=3)
 inhpois <- function(T, fun=NULL, M=NULL) UseMethod("inhpois")
 
 #' @export
 inhpois.default <- function(T, fun=NULL, M=NULL) {
 
-    if (is.null(fun) & is.null(M))
-        stop("One of 'fun' or 'M' must not be 'NULL'")
-
-    if (is.null(fun))
-        fun = function(x) M
-
+    if (is.null(fun) & is.null(M)) stop("One of 'fun' or 'M' must not be 'NULL'")
+    if (is.null(fun)) fun = function(x) M
     if (is.null(M)) {
         warning("M is computed approximately and should be user-specified.")
         M = max( sapply(seq(0, T, length.out = 1e3), fun) )
     }
-
-    # Create full function
 
     # Simulate an homonogeous Poisson on [0,T]x[0,M]
     m <- rpois(1, lambda=M*T)
@@ -117,23 +112,21 @@ plot.inhpois <- function(inhpois, precision=1e3) {
 #' @export
 #'
 #' @examples
-#' # Simulate an exponential Hawkes process with baseline intensity 1, reproduction mean 0.5 and exponential fertility distribution with rate 2.
+#' # Simulate an exponential Hawkes process with baseline intensity 1, reproduction mean 0.5 and exponential fertility function with rate 2.
 #' x <- hawkes(10, M=1, repr=0.5, family="exp", rate=2)
-#' plot(x)
+#' # Simulate a Hawkes process with baseline intensity function 1 + sin(x), reproduction mean 0.5 and custom [0,1]-triangular fertility function.
+#' x <- hawkes(10, fun=function(y) {1+sin(y)}, M=2, repr=0.5, family=function(n) {1 - sqrt(1 - runif(n))})
 hawkes <- function(T, fun=NULL, M=NULL, repr, family="exp", ...) UseMethod("hawkes")
 
 #' @export
 hawkes.default <- function(T, fun=NULL, M=NULL, repr, family="exp", ...) {
 
     # Check if fertility distribution function is user specified or chosen amongst R "r___" functions
-    if (!is.character(family))
-        distname = as.character(match.call()$family)
-    else distname = paste0("r", family)
-
-    if (!exists(distname, mode = "function"))
-        stop(paste0("The ", distname, " function must be defined."))
-
-    distfun = get(distname)
+    if (is.character(family))
+        tryCatch(expr = {distfun = get(paste0("r", family), mode="function")},
+                 error = function(cond) {stop(paste0("Family argument '", family, "' has no corresponding random generation function 'r", family, "'"))})
+    else if (!is.function(family)) stop(paste0("The argument 'family' is not a function."))
+    else distfun = family
 
     # Get immigrants distributed as inhpois
     # and number of descendants for each distributed as Poisson with rate `repr`
@@ -170,7 +163,7 @@ hawkes.default <- function(T, fun=NULL, M=NULL, repr, family="exp", ...) {
     else p <- numeric(0)
 
     # Return object
-    sim <- list(p=p, T=T, repr=repr, distname=distname, distfun=function(n) distfun(n, ...), n=length(p),
+    sim <- list(p=p, T=T, repr=repr, distfun=distfun, family=family, distargs=list(...), n=length(p),
                 immigrants=immigrants, gen=gen, ancestors=ancestors, call=match.call())
     class(sim) <- "hawkes"
     return( sim )
@@ -211,35 +204,119 @@ plot.hawkes <- function(hawkes, intensity=FALSE, precision=1e3) {
     }
 }
 
+#' Intensity of a Hawkes process
+#'
+#' Outputs the intensity of a Hawkes process `x`, given the specified set of parameters.
+#' If the input `x` is of class "hawkes" output by function `hawkes`, the parameters of the simulation will be used to compute the intensity.
+#' If any parameter is user-specified in the function call, it will use these instead.
+#'
+#' @param x Either: a numeric vector, sorted in ascending order; or an object of class "hawkes" output by function `hawkes`.
+#' @param t A numeric value or vector, at which the intensity should be computed.
+#' @param fun (default = NULL) A numeric function - The intensity function of the immigrant process.
+#' @param M (default = NULL) A non-negative numeric value - The maximum bound on `fun` or, if `fun` is `NULL`, constant intensity function of the immigrant process.
+#' @param repr (default = NULL) A non-negative numeric value - The mean number of offsprings.
+#' @param family (default = NULL) A character string "name" naming a distribution with corresponding distribution function `dname`, or directly the distribution function.
+#' @param ... Additional arguments passed on to the random generation function `dname`.
+#'
+#' @return The intensity at time t.
+#'
 #' @export
-intensity <- function(object, t) UseMethod("intensity")
+#'
+#' @examples
+#' # Simulate an exponential Hawkes process with baseline intensity 1, reproduction mean 0.5 and exponential fertility distribution with rate 2.
+#' x <- hawkes(10, M=1, repr=0.5, family="exp", rate=2)
+#' plot(x, intensity = TRUE)
+#' intensity(x, 0:10)
+#' # Intensity under a different model
+#' intensity(x, 0:10, repr=0.8, rate=3)
+#' # Simulate a Hawkes process with baseline intensity function 1 + sin(x), reproduction mean 0.5 and custom [0,1]-triangular fertility function.
+#' x <- hawkes(10, fun=function(y) {1+sin(y)}, M=2, repr=0.5, family=function(n) {1 - sqrt(1 - runif(n))})
+#' plot(x, intensity = TRUE)
+#' intensity(x, 0:10, family=function(y) ifelse(y>0 & y<1, 2-2*y, 0))
+intensity <- function(x, t, fun = NULL, M = NULL, repr = NULL, family = NULL, ...) {
 
-#' @export
-intensity.twinstim <- function(twinstim, t) {
+    # Define pattern
+    if (is.numeric(x)) p = x
+    else if (is(x, "hawkes")) p = x$p
+    else stop("'x' must be numeric.")
 
-    # Compute A
-    if (distname == "rexp") {
+    # Define immigrant intensity function
+    if (!is.null(fun)) {}
+    else if (!is.null(M)) fun = function(y) M
+    else if (is(x, "hawkes")) fun = x$immigrants$fun
+    else stop("One of 'fun' or 'M' must not be 'NULL'.")
+
+    # Define reproduction mean
+    if (!is.null(repr)) {}
+    else if (is(x, "hawkes")) repr = x$repr
+    else stop("'repr' must not be 'NULL'.")
+
+    # Define family
+    if (is.character(family))
+        tryCatch(expr = {distfun = get(paste0("d", family), mode="function")},
+                 error = function(cond) {stop("d", family, " is not a valid density function.")})
+    else if (!is.null(family) & !is.function(family)) stop(paste0("The argument 'family' is not a function."))
+    else if (!is.null(family)) {
+        distfun = family
+        family = deparse(substitute(family))
+    } else if (is(x, "hawkes")) {
+        family = x$family
+        tryCatch(expr = {distfun = get(paste0("d", x$family), mode="function")},
+                 error = function(cond) {stop("d", x$family, " is not a valid density function. Specify one in argument 'family'.")})
+    } else stop("'family' must not be 'NULL'.")
+
+    # Define optional arguments if user-specified (or overwrite if 'x' is of class 'hawkes')
+    if (is(x, "hawkes")) distargs = modifyList(x$distargs, list(...))
+    else distargs = list(...)
+
+    # If outside of bounds, return error
+    if (any(t < 0))
+        stop("t must be non negative.")
+
+    if (family == "exp") {
+
+        rate = ifelse("rate" %in% names(distargs), distargs$rate, 1)
+
+        # Compute A
         if (length(p)== 0) A <- numeric(0)
         if (length(p) > 0) A <- 0
         if (length(p) > 1)
-            for (i in 2:length(p)) A[i] <- exp(-beta * (p[i] - p[i-1])) * (1 + A[i-1])
+            for (i in 2:length(p)) A[i] <- exp(-rate * (p[i] - p[i-1])) * (1 + A[i-1])
+
+        # Create vector of past closest points of hawkes
+        index <- sapply(t, function(j) {
+            Position(function(u) u >= j, p, nomatch=length(p)+1)-1
+        })
+
+        # Endemic part of the intensity
+        int <- sapply(t, fun)
+
+        # Epidemic part
+        pind <- which(index > 0)
+        int[pind] <- int[pind] + repr * rate * (A[index[pind]]+1) * exp(- rate * (t[pind] - p[index[pind]]))
+
+        return(int)
+
+    } else {
+
+        # Create vector of past closest points of hawkes
+        index <- sapply(t, function(j) {
+            Position(function(u) u >= j, p, nomatch=length(p)+1)-1
+        })
+
+        # Endemic part of the intensity
+        int <- sapply(t, fun)
+
+        # Epidemic part
+        pind <- which(index > 0)
+        int[pind] <- int[pind] + repr * sapply(pind, function(s) {
+            sum(sapply( p[1:index[s]], function(q) {do.call(distfun, c(t[s]-q, distargs))} ))
+        })
+
+        return(int)
+
     }
 
-    # If outside of bounds, return error
-    if (any(t < 0) || any(t > twinstim$T))
-        stop("t is out of bound.")
-    # Create vector of past closest points of twinstim
-    index <- sapply(t, function(j) {
-        Position(function(p) p >= j, twinstim$p, nomatch=twinstim$n+1)-1
-    })
-    # Endemic part of the intensity
-    int <- twinstim$immigrants$fun(t)
-    # Epidemic part
-    pind <- which(index > 0)
-    int[pind] <- int[pind] +
-        twinstim$alpha * (twinstim$A[index[pind]]+1) *
-        exp(- twinstim$beta * (t[pind] - twinstim$p[index[pind]]))
-    return( int )
 }
 
 #' @export
