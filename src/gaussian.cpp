a9a21e4dc57691e9ab4bc9e5cd1474f30186fa85
Felix Cheysson <felix@cheysson.fr>
2020-03-05 11:59:26 +0100

    Fixed problem for Gaussian kernel.

--- src/gaussian.cpp
+++ src/gaussian.cpp
@@ -3,7 +3,7 @@
 
 // Model is given by its intensity :
 // Intensity = param(0) + int h(t-u) dN(u)
-// where h(t) = 1 / sqrt(2 * pi * param(3)) * exp(-(t-param(2))^2 / (2 * param(3)))
+// where h(t) = param(1) / sqrt(2 * pi * param(3)) * exp(-(t-param(2))^2 / (2 * param(3)))
 // Then:
 // param(0): baseline intensity of the Hawkes process
 // param(1): reproduction rate /in [0,1]
@@ -21,9 +21,9 @@ double Gaussian::mean() {
 // Methods for time- and frequency-domain excitation functions
 
 arma::vec Gaussian::h( arma::vec x ) {
-    return arma::exp(- .5 * (x - param(2)) % (x - param(2)) / param(3)) / sqrt(2 * arma::datum::pi * param(3));
+    return param(1) * arma::exp(- .5 * (x - param(2)) % (x - param(2)) / param(3)) / sqrt(2 * arma::datum::pi * param(3));
 }
 
 arma::cx_vec Gaussian::H( arma::vec xi ) {
-    return arma::exp(- i * param(2) * xi) % arma::exp(- .5 * param(3) * xi % xi);
+    return param(1) * arma::exp(- i * param(2) * xi) % arma::exp(- .5 * param(3) * xi % xi);
 }
