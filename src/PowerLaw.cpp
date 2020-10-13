508a3a3ee4eba9f9c129b7614a81564502672f69
Felix Cheysson <felix@cheysson.fr>
2020-10-13 17:13:52 +0200

    MLE done for Power Law kernel.

--- src/PowerLaw.cpp
+++ src/PowerLaw.cpp
@@ -19,7 +19,7 @@ double PowerLaw::mean() {
 
 arma::vec PowerLaw::dmean() {
     double denom = 1.0 / ( 1.0 - param(1) );
-    arma::vec grad = { denom, param(0) * denom * denom, 0 };
+    arma::vec grad = { denom, param(0) * denom * denom, 0, 0 };
     return grad;
 }
 
@@ -28,7 +28,7 @@ arma::vec PowerLaw::dmean() {
 
 arma::vec PowerLaw::h( arma::vec x ) {
     return param(1) * param(2) * exp(param(2) * log(param(3))) *
-        arma::exp(-(param(2) + 1) * arma::log(param(3) + x));
+        arma::exp(-(param(2) + 1.0) * arma::log(param(3) + x));
 }
 
 arma::cx_vec PowerLaw::H( arma::vec xi ) {              //todo
@@ -50,19 +50,20 @@ double PowerLaw::loglik() {
     const double mu = param(1);
     const double theta = param(2);
     const double a = param(3);
-    const double atheta = exp(theta * log(a));
+    const double log_a = log(a);
+    const double atheta = exp(theta * log_a);
 
     // Sum log \lambda*
     double part1 = log(eta);
     double hsum;
     for (arma::uword i = 1; i < n; i++) {
-        hsum = arma::accu(arma::exp(-(theta+1) * arma::log(a + events.subvec(0, i-1))));
+        hsum = arma::accu(arma::exp(-(theta+1.0) * arma::log(a + events(i) - events.subvec(0, i-1))));
         part1 += log(eta + mu * theta * atheta * hsum);
     }
 
     // Int \lambda* dt
-    hsum = arma::accu(arma::exp(theta * (log(a) - arma::log(a + T - events))));
-    double part2 = eta * T + mu * (double)n - mu * hsum;
+    hsum = arma::accu(arma::exp(- theta * arma::log(a + T - events)));
+    double part2 = eta * T + mu * (double)n - mu * atheta * hsum;
 
     return part1 - part2;
 };
@@ -75,34 +76,47 @@ arma::vec PowerLaw::dloglik() {
     const double T = data->getTimeEnd() - data->getTimeBegin();
     const double eta = param(0);
     const double mu = param(1);
-    const double beta = param(2);
+    const double theta = param(2);
+    const double a = param(3);
+    const double log_a = log(a);
+    const double atheta = exp(theta * log_a);
 
     // Fill for i = 0
-    arma::vec grad = { 1.0/eta, 0.0, 0.0 };
+    arma::vec grad = { 1.0/eta, 0.0, 0.0, 0.0 };
 
     // Iterate on arrival times
-    double A = 0.0;
-    double C = 0.0;
-    double B;
-    double denom, expint;
+    double hsum, inv_lambda;
     for (arma::uword i = 1; i < n; i++) {
-        expint = exp(- beta * (events(i) - events(i-1)));
-        A = expint * (1.0 + A);
-        C = expint * (events(i-1) + C);
-        B = events(i) * A - C;
-        denom = 1.0 / (eta + mu * beta * A);
-        grad(0) += 1.0 * denom;
-        grad(1) += beta * A * denom;
-        grad(2) += (mu * A - mu * beta * B) * denom;
+        arma::vec diffs = a + events(i) - events.subvec(0, i-1);
+        arma::vec log_diffs = arma::log(diffs);
+        arma::vec diffsmthetam1 = arma::exp(-theta * log_diffs) / diffs;
+        hsum = arma::accu(diffsmthetam1);
+        inv_lambda = 1.0 / (eta + mu * theta * atheta * hsum);
+
+        grad(0) += inv_lambda;
+        grad(1) += inv_lambda * theta * atheta * hsum;
+        grad(2) += inv_lambda * mu * atheta *
+            arma::accu(
+                diffsmthetam1 % (
+                        1.0 + theta * (log_a - log_diffs)
+                )
+            );
+        grad(3) += inv_lambda * mu * theta * (atheta / a) *
+            arma::accu(
+                (theta * (events(i) - events.subvec(0, i-1)) - a) %
+                    diffsmthetam1 / diffs
+            );
     }
 
-    expint = exp(- beta * (T - events(n-1)));
-    A = expint * (1.0 + A);
-    C = expint * (events(n-1) + C);
-    B = T * A - C;
+    arma::vec log_diffs = arma::log(a + T - events);
     grad(0) -= T;
-    grad(1) -= ( (double)n - A );
-    grad(2) -= mu * B;
+    grad(1) -= n - atheta * arma::accu(arma::exp(- theta * log_diffs));
+    grad(2) += mu * atheta * arma::accu(
+        (log_a - log_diffs) % arma::exp(-theta * log_diffs)
+    );
+    grad(3) += mu * theta * exp((theta - 1.0) * log_a) * arma::accu(
+        (T - events) % arma::exp(-(theta + 1.0) * log_diffs)
+    );
 
     return grad;
 };
@@ -115,38 +129,55 @@ Rcpp::List PowerLaw::loglikngrad() {
     const double T = data->getTimeEnd() - data->getTimeBegin();
     const double eta = param(0);
     const double mu = param(1);
-    const double beta = param(2);
+    const double theta = param(2);
+    const double a = param(3);
+    const double log_a = log(a);
+    const double atheta = exp(theta * log_a);
 
     // Fill for i = 0
     double lik = log(eta);
-    arma::vec grad = { 1.0/eta, 0.0, 0.0 };
+    arma::vec grad = { 1.0/eta, 0.0, 0.0, 0.0 };
 
     // Iterate on arrival times
-    double A = 0.0;
-    double C = 0.0;
-    double B;
-    double denom, expint;
+    double hsum, inv_lambda;
     for (arma::uword i = 1; i < n; i++) {
-        expint = exp(- beta * (events(i) - events(i-1)));
-        A = expint * (1.0 + A);
-        C = expint * (events(i-1) + C);
-        B = events(i) * A - C;
-        denom = 1.0 / (eta + mu * beta * A);
-        lik += log(eta + mu * beta * A);
-        grad(0) += 1.0 * denom;
-        grad(1) += beta * A * denom;
-        grad(2) += (mu * A - mu * beta * B) * denom;
+        arma::vec diffs = a + events(i) - events.subvec(0, i-1);
+        arma::vec log_diffs = arma::log(diffs);
+        arma::vec diffsmthetam1 = arma::exp(-theta * log_diffs) / diffs;
+        hsum = arma::accu(diffsmthetam1);
+        inv_lambda = 1.0 / (eta + mu * theta * atheta * hsum);
+
+        lik += log(eta + mu * theta * atheta * hsum);
+
+        grad(0) += inv_lambda;
+        grad(1) += inv_lambda * theta * atheta * hsum;
+        grad(2) += inv_lambda * mu * atheta *
+            arma::accu(
+                diffsmthetam1 % (
+                        1.0 + theta * (log_a - log_diffs)
+                )
+            );
+        grad(3) += inv_lambda * mu * theta * (atheta / a) *
+            arma::accu(
+                (theta * (events(i) - events.subvec(0, i-1)) - a) %
+                    diffsmthetam1 / diffs
+            );
     }
 
-    // Likelihood of non occurrence
-    expint = exp(- beta * (T - events(n-1)));
-    A = expint * (1.0 + A);
-    C = expint * (events(n-1) + C);
-    B = T * A - C;
-    lik -= eta * T + mu * ((double)n - A);
+    // Compensator part
+    arma::vec log_diffs = arma::log(a + T - events);
+    hsum = arma::accu(arma::exp(- theta * log_diffs));
+
+    lik -= eta * T + mu * (double)n - mu * atheta * hsum;
+
     grad(0) -= T;
-    grad(1) -= ( (double)n - A );
-    grad(2) -= mu * B;
+    grad(1) -= n - atheta * arma::accu(arma::exp(- theta * log_diffs));
+    grad(2) += mu * atheta * arma::accu(
+        (log_a - log_diffs) % arma::exp(-theta * log_diffs)
+    );
+    grad(3) += mu * theta * exp((theta - 1.0) * log_a) * arma::accu(
+        (T - events) % arma::exp(-(theta + 1.0) * log_diffs)
+    );
 
     return Rcpp::List::create(Rcpp::Named("objective") = lik, Rcpp::Named("gradient") = grad);
 };
